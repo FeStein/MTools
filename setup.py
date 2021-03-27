1c20e11faf36f98a29b6d34a59ddcc5b30ee6e86
FelSt <63238040+FeStein@users.noreply.github.com>
2021-03-27 02:07:21 +0100

    Adding Abaqus Parser and some package related stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='MTools',
+                 version='0.0.1',
+                 description='Collection of modules I use to work with
+                 simulation tools in an efficient way.',
+                 author='Felix Steinmetz',
+                 author_email='fsteinme@rhrk.uni-kl.de',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
