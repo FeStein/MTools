1b616903862315d2805b55c2fe0ce720b58eb651
FelSt <63238040+FeStein@users.noreply.github.com>
2021-03-27 02:11:58 +0100

    Resolving error in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ import setuptools
 
 setuptools.setup(name='MTools',
                  version='0.0.1',
-                 description='Collection of modules I use to work with
-                 simulation tools in an efficient way.',
+                 description='Collection of modules I use to work with simulation tools in an efficient way.',
                  author='Felix Steinmetz',
                  author_email='fsteinme@rhrk.uni-kl.de',
                  packages=setuptools.find_packages(),
